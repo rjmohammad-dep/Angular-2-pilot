05dda999f8aa2f78211bc8cf77127b8529cc9195
RJ Mohammad <rjmohammad@outlook.com>
2017-04-26 23:05:17 -0400

    first tutorail finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+
 export class Hero {
   id: number;
   name: string;
@@ -13,12 +14,12 @@ export class Hero {
   <div>
     <label>name: </label>
     <input [(ngModel)]="hero.name" placeholder="name">
-    </div>`,
+  </div>`,
 })
 export class AppComponent {
   title = 'Tour of Heroes';
   hero: Hero = {
     id: 1,
-    name: 'WindStrom'
+    name: 'Windstrom'
   }
 }
